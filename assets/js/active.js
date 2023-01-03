579c62c80a2adce22184f058bda9c2484402828c
RajuAhmed2 <92273686+RajuAhmed2@users.noreply.github.com>
2023-01-03 19:48:05 +0600

    first commit

--- assets/js/active.js
+++ assets/js/active.js
@@ -6,37 +6,16 @@ Author: RRDevs
 (function($) {
     "use strict";
 
-    $(document).ready( function() {
-
-        // /*** Sticky header */
-        // $(window).scroll(function(){
-        //     if($("body").scrollTop() > 0 || $("html").scrollTop() > 0) {
-        //         $("header").addClass("stop");
-        //     } else {
-        //         $("header").removeClass("stop");
-        //     }
-		// });
-		// $(window).scroll(function(){
-        //     if($("body").scrollTop() > 0 || $("html").scrollTop() > 0) {
-        //         $("header").addClass("stop");
-        //     } else {
-        //         $("header").removeClass("stop");
-        //     }
-		// });
-		// $(function(){
-		// 	$('#topbar').scrollupbar();
-		// });
+	$(document).ready(function () {
 			
 		
         /*** mobile menu  */
 		$("#hamburger").on("click", function () {
 			$(".mobile-nav").addClass("show");
-			$(".offcanvas-overlay").addClass("overlay-open");
 		});
 
         $(".close-nav").on("click", function () {
 			$(".mobile-nav").removeClass("show");
-			$(".offcanvas-overlay").removeClass("overlay-open");
 		});
 
 		$(window).scroll(function(){
